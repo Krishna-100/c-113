ed3f29576bd660cf9379f8fe5dcac12ebdc990c8
Krishna-100 <107849496+Krishna-100@users.noreply.github.com>
2022-07-12 14:17:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
-function preload() {
-
+function preload(){
 }
 
 function setup() {
-    canvas = createCanvas(640, 480);
-    canvas.position(110, 250);
+    canvas = createCanvas(500,400);
+    canvas.position(70,200);
     video = createCapture(VIDEO);
     video.hide();
-
-    tint_color = "";
 }
 
 function draw() {
-    image(video, 0, 0, 640, 480);
-    tint(tint_color);
+    image(video,100,80,300,250);
+
+    stroke(168, 10, 10);
+    fill(168, 10, 10);
+    
+    circle(40,40,50);
+    circle(460,40,50);
+    circle(40,360,50);
+    circle(460,360,50);
+
+
+    stroke(50, 168, 82);
+    fill(50, 168, 82);
+
+    rect(63, 27, 375, 25);
+    rect(63, 347, 375, 25);
+    rect(27, 63, 25, 275);
+    rect(448, 63, 25, 275);
 }
 
-function take_snapshot() {
-    save('student_name.png');
-}
-
-function filter_tint()
-{
-    tint_color = document.getElementById("color_name").value;
+function take_snapshot(){
+    save("MyImageframe.png");
 }
